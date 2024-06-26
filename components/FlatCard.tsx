2a940a6ed63a00637059915dcdd17790e9996b90
Shubham <chaudhary2001shubham@gmail.com>
2024-06-27 02:06:12 +0530

    Added FlatCard

---
+++ components/FlatCard.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const FlatCard = (): JSX.Element => {
+    return (
+        <View>
+            <Text>FlatCard</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+
+export default FlatCard;
